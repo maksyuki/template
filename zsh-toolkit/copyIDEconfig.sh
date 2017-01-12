ce3b2a166f3e3974e29d1a097f99cfcf1981dd35
maksyuki <myc20404401@sina.com>
2017-01-12 17:08:34 +0800

    Format the 'copyIDEconfig.sh'

--- zsh-toolkit/copyIDEconfig.sh
+++ zsh-toolkit/copyIDEconfig.sh
@@ -8,42 +8,40 @@ echo "The target path is $gitpath"
 echo "The target path is $gitpath_vim_linux"
 echo "..."
 
-if [ ! -x $gitpath ]; then
-    echo "no such path or the path is permission denied "; exit 1;
-fi
-
-if [ ! -x $gitpath_vim_linux ]; then
-    echo "no such path or the path is permission denied "; exit 1;
+if [[ ! -x $gitpath ]]; then
+    echo "no such path or the path is permission denied "; exit 1
+elif [[ ! -x $gitpath_vim_linux ]]; then
+    echo "no such path or the path is permission denied "; exit 1
 fi
 
 if ! cp -Rf ~/.atom $gitpath; then
-    echo "copy .atom failed"; exit 1;
+    echo "copy .atom failed"; exit 1
 else
-    echo "copy .atom finished";
+    echo "copy .atom finished"
 fi
 
 if ! cp -Rf ~/.oh-my-zsh $gitpath; then
-    echo "copy .oh-my-zsh failed"; exit 1;
+    echo "copy .oh-my-zsh failed"; exit 1
 else
-    echo "copy .oh-my-zsh finished";
+    echo "copy .oh-my-zsh finished"
 fi
 
 if ! cp -Rf ~/.config/QtProject $gitpath; then
-    echo "copy QtProject failed"; exit 1;
+    echo "copy QtProject failed"; exit 1
 else
-    echo "copy QtProject finished";
+    echo "copy QtProject finished"
 fi
 
 if ! cp /etc/vim/vimrc $gitpath_vim_linux; then
-    echo "copy vimrc failed"; exit 1;
+    echo "copy vimrc failed"; exit 1
 else
-    echo "copy vimrc finished";
+    echo "copy vimrc finished"
 fi
 
 if ! cp /usr/share/vim/vim74/colors/monokai.vim $gitpath_vim_linux; then
-    echo "copy monokai color theme failed"; exit 1;
+    echo "copy monokai color theme failed"; exit 1
 else
-    echo "copy monokai color theme finished";
+    echo "copy monokai color theme finished"
 fi
 
 echo "..."
