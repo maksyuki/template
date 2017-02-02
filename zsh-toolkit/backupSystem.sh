c5ffe5d1d056b9a1790a1aeee972dbdad42ce570
maksyuki <myc20404401@sina.com>
2017-02-02 20:04:38 +0800

    Add root judge function and modify status output

--- zsh-toolkit/backupSystem.sh
+++ zsh-toolkit/backupSystem.sh
@@ -8,39 +8,49 @@ all="backup_"$arch"_"$datename
 #echo $all
 backpath="/"
 
+if [[ $(whoami) = "root" ]]; then
+    echo "This is root"
+else
+    echo "Need run this shell in root(execute 'sudo su' command)"; exit 1
+fi
+
 echo "Are you sure to backup system into $all.tgz? [Y/n]"
 read is_sure
 
 #can not have opinion after -f
 if [[ $is_sure = [Yy] ]]; then
-    sudo su
     cd /
-    echo "Do you want to start backup right now? [Y/n]"
+    echo "The cur path is $(pwd), do you want to start backup right now? [Y/n]"
     read is_start
     if [[ $is_start = [Yy] ]]; then
         tar -pczvf $all.tgz --exclude=/proc --exclude=/lost+found --exclude=/$all.tgz --exclude=/mnt --exclude=/sys --exclude=/media $backpath
-        exit
+        echo "========"
+        echo "..."
+        echo "All done"
+        echo "Remember execute 'exit' to log out"
+        echo "========"
     elif [[ $is_start = [Nn] ]]; then
-        exit
-        echo
+        echo "=================="
         echo "Cancel backup task"
         echo "..."
-        echo "All done"; exit 1
+        echo "All done"
+        echo "=================="; exit 1
     else
-        exit
+        echo "============="
         echo "..."
-        echo "Invalid input"; exit 1
+        echo "Invalid input"
+        echo "============="; exit 1
     fi
 elif [[ $is_sure = [Nn] ]]; then
-    echo
+    echo "=================="
     echo "Cancel backup task"
     echo "..."
-    echo "All done"; exit 1
+    echo "All done"
+    echo "=================="; exit 1
 else
-    echo
+    echo "============="
     echo "..."
-    echo "Invalid input"; exit 1
+    echo "Invalid input"
+    echo "============="; exit 1
 fi
 
-echo "..."
-echo "All done"
