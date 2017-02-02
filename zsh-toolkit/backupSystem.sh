6f9845485ab6372ee52dee0a2252975db6673afa
maksyuki <myc20404401@sina.com>
2017-02-02 18:48:27 +0800

    Add backup root system function

--- zsh-toolkit/backupSystem.sh
+++ zsh-toolkit/backupSystem.sh
@@ -6,15 +6,34 @@ arch=$(uname -n)
 arch=$arch"_"$(uname -m)
 all="backup_"$arch"_"$datename
 #echo $all
-backpath="/home/maksyuki/Desktop/maksyuki"
+backpath="/"
 
 echo "Are you sure to backup system into $all.tgz? [Y/n]"
 read is_sure
 
+#can not have opinion after -f
 if [[ $is_sure = [Yy] ]]; then
-    tar -cvpzf $all.tgz $backpath
+    sudo su
+    cd /
+    echo "Do you want to start backup right now? [Y/n]"
+    read is_start
+    if [[ $is_start = [Yy] ]]; then
+        tar -pczvf $all.tgz --exclude=/proc --exclude=/lost+found --exclude=/$all.tgz --exclude=/mnt --exclude=/sys --exclude=/media $backpath
+        exit
+    elif [[ $is_start = [Nn] ]]; then
+        exit
+        echo
+        echo "Cancel backup task"
+        echo "..."
+        echo "All done"; exit 1
+    else
+        exit
+        echo "..."
+        echo "Invalid input"; exit 1
+    fi
 elif [[ $is_sure = [Nn] ]]; then
     echo
+    echo "Cancel backup task"
     echo "..."
     echo "All done"; exit 1
 else
