6eefddc64031b267a04cf98d2f423646c6163069
maksyuki <myc20404401@sina.com>
2020-03-30 14:51:45 +0800

    Add allegro skill files

---
+++ allegro_pcb_design_gxl_16.6_2015/MDSkill/install.bat
@@ -0,0 +1,5 @@
+echo It's Starting Install MDSkill，，，
+echo It's Install  file，，，
+echo  load("C:/Users/abcd/Documents/GitHub/template/allegro_pcb_design_gxl_16.6_2015/MDSkill/MDSkill_main.il") >> "%CDSROOT%\share\local\pcb\skill\allegro.ilinit"
+echo ===================================
+echo Install completed�� 
